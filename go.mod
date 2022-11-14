7a44cd2bd729739570c6f26a95c982cff541cb7c
mjshodiq31 <muhammadjs31@gmail.com>
2022-11-14 09:31:17 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/shdqjfr31/app-say-hello
+
+go 1.19
+
+require github.com/shdqjfr31/say-hello-modules v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	say_hello_modules "github.com/shdqjfr31/say-hello-modules"
+)
+
+func main() {
+	fmt.Println(say_hello_modules.SayHello())
+}
